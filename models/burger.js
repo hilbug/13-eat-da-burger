91e8424142fcff8b8e0eee7ffcf45c5577b9677d
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-13 12:45:51 -0400

    add models burger.js

---
+++ models/burger.js
@@ -0,0 +1,22 @@
+const orm = require('../config/orm');
+
+// Call orm method, passing in the anonymous function(with "res") as the callback.
+orm.selectAll("parties").then((result) => {
+    console.log(result);
+});
+
+orm.insertOne("burgers", cols, vals).then((result) => {
+    console.log(result);
+});
+
+orm.updateOne("burgers", objColVals, condition).then((result) => {
+    console.log(result);
+});
+
+orm.deleteOne("burgers", condition).then((result) => {
+    console.log(result);
+});
+
+// what do we export out of this file??
+// Export the database functions for the controller (catsController.js).
+//module.exports = burger;
