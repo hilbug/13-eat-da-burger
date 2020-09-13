d598be3018b71114766b8832aac07fca43e9410a
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-13 10:24:51 -0400

    sql schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Susielicious Curdicious Burger');
+INSERT INTO burgers (burger_name) VALUES ('Toodle Doodle Burger');
+INSERT INTO burgers (burger_name) VALUES ('Zuma Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Magi-Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hogwards Express Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('On a Roll Paw Patrol Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+
+-- insert queries to populate `burgers` table with > three entries
+-- cols: burger_name, devoured
