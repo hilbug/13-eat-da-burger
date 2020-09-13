d181e5bcb1e9ef755682b702b9ad22c0796594b9
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-13 10:52:36 -0400

    orm insertOne

--- config/orm.js
+++ config/orm.js
@@ -1,32 +1,89 @@
 const connection = require('./connection');
 
-const orm = {
-    selectAll: function(tableInput, colToSearch, valOfCol) {
-      return new Promise((resolve, reject) => {
-        var queryString = "SELECT * FROM ??";
-        
-        connection.query(queryString, [burgers], function(err, result) {
-          if (err) reject(err);
-          resolve(result);
-        });
-      });
+/////// from Cats 13-3 exercise #17 //////////
+const printQuestionMarks = (num) => {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
     }
-  };
-  
-  module.exports = orm;
-  
+
+    return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {burger_name: 'Cheeseburger'} => ["burger_name='Lana Del Grey'"]
+            // e.g. {devoured: true} => ["devoured=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+/////////////////////////////////////////////////////////////////////////////
+
+const orm = {
+    selectAll: function (tableInput) {
+        return new Promise((resolve, reject) => {
+            var queryString = "SELECT * FROM ??";
+
+            connection.query(queryString, [tableInput], function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+    },
+    insertOne: function (table, cols, vals) {
+        return new Promise((resolve, reject) => {
+            var queryString = "INSERT INTO " + table;
+
+            queryString += " (";
+            queryString += cols.toString();
+            queryString += ") ";
+            queryString += "VALUES (";
+            queryString += printQuestionMarks(vals.length);
+            queryString += ") ";
+
+            console.log(queryString);
+
+            connection.query(queryString, vals, function (err, result) {
+                if (err) reject(err);
+                resolve(result);
+            });
+        });
+    }
+};
 
 
 
 
-/* * In the `orm.js` file, create the methods that will execute the necessary 
-MySQL commands in the controllers. These are the methods you will need to use 
+module.exports = orm;
+
+
+
+
+
+/* * In the `orm.js` file, create the methods that will execute the necessary
+MySQL commands in the controllers. These are the methods you will need to use
 in order to retrieve and store data in your database.
-​
+
      * `selectAll()`
      * `insertOne()`
      * `updateOne()`
      * `deleteOne()`
-​
+
    * Export the ORM object in `module.exports`.
    */
