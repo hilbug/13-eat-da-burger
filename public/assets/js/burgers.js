4c5e9958074fdc0597b0ba5c9c7269bf607579e7
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-14 09:30:36 -0400

    add burgers.js outline

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,17 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+// Submit - form data to post - insertOne
+    // let newBurger = { name: burgerBox, devoured: 0 }
+    // type: POST  data: newBurger
+
+// Devoured button - put - updateOne to move devoured to 1
+    // need: data-id and data-devoured in handlebars button
+    // devoured button clicked - newDevourState = devoured: 1
+    // type: PUT data: newDevourState
+
+// Delete button - delete - deleteOne
+    // need: data-id
+    // DELETE - type: delete
+
+// Non-devoured list - get - selectAll and filter non-devoured and devoured or does this happen in handlebars?
+});
