e91bdc1376ca2b2a743d80abb36b447f5aec2b5c
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-16 00:26:42 -0400

    add process.env.JAWSDB_URL for heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
+const connection;
 
-const connection = mysql.createConnection({
-	host:"localhost",
-	user:"root",
-	password:"root12345",
-	database:"burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "root12345",
+		database: "burgers_db"
+	});
+}
 
 module.exports = connection;
