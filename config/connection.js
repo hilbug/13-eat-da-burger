ae143fb171cc429b6d9593160e7d372fb02f0ca7
hilbug <65197724+hilbug@users.noreply.github.com>
2020-09-16 08:24:23 -0400

    add connection.connect()

--- config/connection.js
+++ config/connection.js
@@ -12,4 +12,13 @@ if (process.env.JAWSDB_URL) {
 	});
 }
 
+// Make connection.
+connection.connect(function(err) {
+	if (err) {
+	  console.error("error connecting: " + err.stack);
+	  return;
+	}
+	console.log("connected as id " + connection.threadId);
+  });  
+
 module.exports = connection;
